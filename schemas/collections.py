99cb24f31b5eecf5215eb788c8735ff0ffc82f4a
zxcumarik <nataliastroman09@gmail.com>
2025-02-01 16:22:51 +0200

    'create_models'

---
+++ schemas/collections.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class CollectionBase(BaseModel):
+    name: str = Field(max_length=100) 
+    user_id: str
+
+
+class CollectionCreate(BaseModel):
+    pass
+
+
+class CollectionUpdate(BaseModel):
+    name: Optional[str]
+    user_id: Optional[int]
+
+
+class CollectionPublic(CollectionCreate):
+    id: int
