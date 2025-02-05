3e68bd71a06579bd078f1f73886d3b0b78ab5b33
Crais2905 <slisovets2905@gmail.com>
2025-02-05 16:44:55 +0200

    get_one and delete item

--- app/api/item/item.py
+++ app/api/item/item.py
@@ -7,7 +7,7 @@ from utils.filters import item_filters
 router = APIRouter()
 
 
-@router.post('/', response_model=ItemPublic, status_code=201)
+@router.post('/', response_model=ItemPublic, status_code=status.HTTP_201_CREATED)
 async def create_item(item_data: ItemCreate, item_crud: ItemCrud = Depends(ItemCrud)):
     result = await item_crud.create_item(item_data)
     return result
@@ -22,3 +22,24 @@ async def get_items(
 ):
     result = await item_crud.get_items(offset, limit, filters)
     return result
+
+
+@router.get('/{item_id}', response_model=ItemPublic)
+async def get_itenm(item_id: int, item_crud: ItemCrud = Depends(ItemCrud)):
+    item = await item_crud.get_item(item_id=item_id)
+    if not item:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+        )
+    return item
+
+
+@router.delete('/{item_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item(item_id: int, item_crud: ItemCrud = Depends(ItemCrud)):
+    item = await item_crud.get_item(item_id=item_id)
+    if not item:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+        )
+    await item_crud.delete_item(item_id)
+    return
